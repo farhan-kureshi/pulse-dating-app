2a1b16a3e73fe1a1f07bfa1e01216fb7c5b5f19f
farhan-kuresh <kureshif624@gmail.com>
2026-05-15 21:29:52 +0530

    Fixed CORS policy for Vercel

--- pulsedate_backend/pulsedate_backend/settings.py
+++ pulsedate_backend/pulsedate_backend/settings.py
@@ -64,6 +64,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173", # 👈 NAYA: IP address bhi allow kiya
     "http://localhost:3000", 
     "http://127.0.0.1:3000",
+    "https://pulse-dating-app.vercel.app",
 ]
 ROOT_URLCONF = 'pulsedate_backend.urls'
 
